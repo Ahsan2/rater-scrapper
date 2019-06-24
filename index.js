4ffd2a5258949ebf98ba68d2b6ca5c2eb3e5ccc9
Ahsan2 <ahsan21916@gmail.com>
2019-06-24 23:53:04 +0500

    misc

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
     await driver.get('https://fmap.citizensfla.com/fmap/login.do');
     let title1 = await driver.getTitle();
     console.log(title1)
-    driver.findElement(By.name('loginId')).sendKeys('W280572');
-    driver.findElement(By.name('password')).sendKeys('JF2019');
-    driver.findElement(By.name('Login')).click();
+    await driver.findElement(By.name('loginId')).sendKeys('W280572');
+    await driver.findElement(By.name('password')).sendKeys('JF2019');
+    await driver.findElement(By.name('Login')).click();
     let title2 = await driver.getTitle();
     console.log(title2)
   } finally {
