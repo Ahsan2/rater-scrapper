d954c8bbaa92629b63c69d98130686397621d621
Ahsan2 <ahsan21916@gmail.com>
2019-06-24 22:58:55 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+ 
+(async function example() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get('https://fmap.citizensfla.com/fmap/login.do');
+    let title1 = await driver.getTitle();
+    console.log(title1)
+    driver.findElement(By.name('loginId')).sendKeys('W280572');
+    driver.findElement(By.name('password')).sendKeys('JF2019');
+    driver.findElement(By.name('Login')).click();
+    let title2 = await driver.getTitle();
+    console.log(title2)
+  } finally {
+    await driver.quit();
+  }
+})()
