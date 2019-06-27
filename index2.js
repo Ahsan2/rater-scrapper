da4dc4a09cc31ca8c231741fa9fd9d9b34279a6a
Ahsan2 <ahsan21916@gmail.com>
2019-06-27 21:58:07 +0500

    merge before pull

--- index2.js
+++ index2.js
@@ -10,7 +10,7 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
     await driver.findElement(By.name('email')).sendKeys('ahsan21916@gmail.com');
     await driver.findElement(By.name('password')).sendKeys('00.Aloha1.');
     await driver.findElement(By.id('000-button_user-login_sign-in-form_log-in')).click();
-    await driver.wait(until.titleIs('Website list'), 1000);
+    await driver.wait(until.titleIs('Website list'), 4000);
     let title2 = await driver.getTitle();
     console.log(title2)
   } finally {
