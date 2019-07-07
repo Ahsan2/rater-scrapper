b943421d01d5d80b98c22e43baa74017c325d3f9
Ahsan2 <ahsan21916@gmail.com>
2019-07-08 01:16:15 +0500

    misc

---
+++ sendMail.js
@@ -0,0 +1,31 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update(require('./config').SES);
+
+const ses = new AWS.SES({ apiVersion: "2010-12-01" });
+
+module.exports = (emailTemplate) => {
+  const params = {
+    Destination: {
+    //   ToAddresses: ["jose.friman@gmail.com"]
+      ToAddresses: ["ahsan21916@gmail.com"]
+    },
+    Message: {
+      Body: {
+        Html: {
+          // HTML Format of the email
+          Charset: "UTF-8",
+          Data:
+            emailTemplate
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "FMAP Lead"
+      }
+    },
+    Source: "jose.friman@gmail.com"
+  };
+  
+  return ses.sendEmail(params).promise();
+}
